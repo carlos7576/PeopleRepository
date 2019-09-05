1746f231ef2fa0f3dd3ccf63d796c198da810d88
CDS2UNIVO11 <CDS2UNIVO11@CDS2UNIVO11>
2019-09-05 09:09:57 -0600

    TABLE

---
+++ People/People/People/PersonReposityCRUD.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SQLite;
+using People.models;
+
+namespace People
+{
+    class PersonReposityCRUD
+    {
+        SQLiteConnection conn;
+        public string statusMessegue { get; set; }
+        public PersonReposityCRUD(string dbPach)
+        {
+            conn = new SQLiteConnection(dbPach);
+            conn.CreateTable<Person>();
+        }
+    }
+}
