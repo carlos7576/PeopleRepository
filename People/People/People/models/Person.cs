0745544614a14fe4974ab42dd9bcfd794f256d31
CDS2UNIVO11 <CDS2UNIVO11@CDS2UNIVO11>
2019-09-05 08:43:29 -0600

    Models class are

---
+++ People/People/People/models/Person.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SQLite;
+
+namespace People.models
+{
+    [Table("T_People")]
+   public class Person
+    {
+        //miembro: propierties
+        [PrimaryKey, AutoIncrement]
+        public int id { get; set; }
+        [MaxLength(50)]
+        public string name { get; set; }
+    }
+}
